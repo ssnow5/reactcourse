0e5690a3712087696f69df3c651ffb62046ef902
David Tan <ssnow5@yahoo.com>
2024-10-30 22:47:50 +0800

    V18

--- 25 NextJS App Router Introduction/foodies-starting-project/05-onwards-foodies-starting-project/app/meals/page.js
+++ 25 NextJS App Router Introduction/foodies-starting-project/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,4 +1,27 @@
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+import Link from 'next/link';
+
 export default function MealsPage() {
-  console.log('Meals Page');
-  return <h1>Meals Page</h1>;
+  // console.log('Meals Page');
+  // return <h1>Meals Page</h1>;
+
+  return (
+    <>
+      <header className={classes.header}></header>
+      <h1>
+        Delicious meals, created{' '}
+        <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>
+        Choose your facourite receipe and cook it yourself. It is easy and fun.
+      </p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">Share Your Favourite Receipe</Link>
+      </p>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
