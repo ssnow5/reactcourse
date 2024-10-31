e7ca3ed6b821f806372fe2891bba63226f9a7822
David Tan <ssnow5@yahoo.com>
2024-10-31 15:57:41 +0800

    V19

--- 25 NextJS App Router Introduction/foodies-starting-project/05-onwards-foodies-starting-project/app/meals/page.js
+++ 25 NextJS App Router Introduction/foodies-starting-project/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,6 +1,13 @@
 import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   // console.log('Meals Page');
@@ -20,7 +27,11 @@ export default function MealsPage() {
         <Link href="/meals/share">Share Your Favourite Receipe</Link>
       </p>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
