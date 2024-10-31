d572b4d16ad9b8d351979a18c3196ee6299d1fea
David Tan <ssnow5@yahoo.com>
2024-11-01 06:27:24 +0800

    V22

--- 25 NextJS App Router Introduction/foodies-starting-project/05-onwards-foodies-starting-project/lib/actions.js
+++ 25 NextJS App Router Introduction/foodies-starting-project/05-onwards-foodies-starting-project/lib/actions.js
@@ -3,7 +3,11 @@
 import { saveMeal } from './meals';
 import { redirect } from 'next/navigation';
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -13,6 +17,21 @@ export async function shareMeal(formData) {
     creator_email: formData.get('email'),
   };
 
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    //throw new Error('Invalid input');
+    return {
+      message: 'Invalid input.',
+    };
+  }
   //    console.log(meal);
   await saveMeal(meal);
   redirect('/meals');
