ea8f7e0a4c7db8b1b06242c28247cb231155f3f5
ssnow5 <ssnow5@yahoo.com>
2024-11-09 21:04:25 +0800

    V33

---
+++ 27 Patterns/01-starting-project/01-starting-project/src/App.jsx
@@ -0,0 +1,50 @@
+import Accordion from "./components/Accordion/Accordion";
+// import AccordionItem from "./components/Accordion/AccordionItem";
+
+function App() {
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <Accordion.Item className="accordion-item">
+            <Accordion.Title className="accordion-item-title" id="experience">
+              We got 20 years of experience
+            </Accordion.Title>
+            <Accordion.Content
+              className="accordion-item-content"
+              id="experience"
+            >
+              <article>
+                <p>You can&apos; go wrong with us.</p>
+                <p>
+                  We are in the business of planning highly individualized
+                  vocation trips for more than 20 years.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item className="accordion-item">
+            <Accordion.Title className="accordion-item-title" id="local-guides">
+              We are working with local guides
+            </Accordion.Title>
+            <Accordion.Content
+              className="accordion-item-content"
+              id="local-guides"
+            >
+              <article>
+                <p>We are not doing this along from our office</p>
+                <p>
+                  Instead, we are working with local guides to ensure a safe and
+                  pleasant vocation.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ 27 Patterns/01-starting-project/01-starting-project/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,50 @@
+import { useContext, useState } from "react";
+import { createContext } from "react";
+import AccordionItem from "./AccordionItem";
+import AccordionTitle from "./AccordionTitle";
+import AccordionContent from "./AccordionContent";
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+
+  if (!ctx) {
+    throw new Error(
+      "Accordion-related components must be warrped by <Accordion>."
+    );
+  }
+
+  return ctx;
+}
+
+export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState();
+
+  function toggleItem(id) {
+    setOpenItemId((prevId) => (prevId === id ? null : id));
+  }
+
+  //   function openItem(id) {
+  //     setOpenItemId(id);
+  //   }
+
+  //   function closeItem() {
+  //     setOpenItemId(null);
+  //   }
+
+  const contextValue = {
+    openItemId,
+    toggleItem,
+  };
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
+}
+
+Accordion.Item = AccordionItem;
+Accordion.Title = AccordionTitle;
+Accordion.Content = AccordionContent;
